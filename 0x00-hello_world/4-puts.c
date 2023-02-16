ec06773845e4c711e06c9a4d6d552f772a826717
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-16 07:30:38 -0800

    4 for the F time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * Guess What !?? This is a Description
+ * main - Guess What !!??  This is a Description
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
