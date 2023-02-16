aeb5754d4f8ae817b1a5e495604414f9f6e0517e
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-16 06:52:17 -0800

    4 for the third time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Guess What !?? This is a Description
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
