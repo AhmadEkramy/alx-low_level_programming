a5743619abcc44cdc7e86abf7f39a7b6e6c031a7
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-17 11:42:36 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print Numbers From 0 To 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int numbers;
+
+for (numbers = 0; numbers <= 10; numbers++)
+{
+printf("%d", numbers);
+}
+putchar('\n');
+return (0);
+}
