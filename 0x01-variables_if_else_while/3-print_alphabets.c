e2d9efe8022bd5c5601c76326ef6b685e9635821
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-17 11:11:01 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print Alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int loweralphabet , ubberalphabet;
+
+	for (loweralphabet = 'a'; loweralphabet <= 'z'; loweralphabet++)
+	{
+		putchar(loweralphabet);
+	}
+	for (ubberalphabet = 'A'; ubberalphabet <= 'Z'; ubberalphabet++)
+	{
+		putchar(ubberalphabet);
+	}
+	putchar('\n');
+return (0);
+}
