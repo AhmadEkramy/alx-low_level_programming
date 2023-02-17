e167da3f49d46cf80cb799abeb00b58aa294e65e
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-17 11:59:12 -0800

    Task 7 For The 2nd Time

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,12 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-        int alphabet;
-
-        for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
-        {
-                putchar(alphabet);
-        }
-        putchar('\n');
-return (0);
-}
+	{
+	int alphabet;
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+	}
