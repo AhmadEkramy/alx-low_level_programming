ab8797c1c2aef93d7815fa959ac04837a7a5ae05
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-21 13:08:58 -0800

    Ahhhh Engineering

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - this is code yeah
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
