d5cffcf32bc76411ff6423c0605ff7dc2c18546f
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-23 12:58:56 -0800

    Ahaaaa

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,9 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
