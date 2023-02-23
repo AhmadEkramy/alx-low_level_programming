9c3a1b46a7ed290b26e3d71ae8dbafe2349c59aa
AhmadEkramy <ahmedekramyabdellatif@gmail.com>
2023-02-23 13:01:27 -0800

    W

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - What a Loop!!
- * Return: 0
- */
+* main - What An Infinity Loop
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+/*
+*	while (i < 10)
+*	{
+*		putchar(i);
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
